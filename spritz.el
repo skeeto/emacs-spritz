d2dab6b483acfd9510ae274c6e65d171148995e6
Christopher Wellons <wellons@nullprogram.com>
2014-10-31 14:24:03 -0400

    Macro it up.

--- spritz.el
+++ spritz.el
@@ -1,28 +1,52 @@
 ;;; -*- lexical-binding: t; -*-
 
+;; absorb
+;; absorb-byte b
+;; absorb-nibble x
+;; absorb-stop
+;; shuffle
+;; whip r
+;; crush
+;; squeeze r
+;; drip
+;; update
+;; output
+
 (require 'cl)
 
 (cl-defstruct (spritz (:constructor spritz--create)
                       (:copier nil)
                       (:type vector)) ; disable type checking
-  (s (vconcat (number-sequence 0 255)))
-  (i 0)
-  (j 0)
-  (k 0)
-  (a 0)
-  (z 0)
-  (w 1))
+  (-s (vconcat (number-sequence 0 255)))
+  (-i 0)
+  (-j 0)
+  (-k 0)
+  (-a 0)
+  (-z 0)
+  (-w 1))
 
-(defmacro spritz--body (var &rest body)
+(defmacro spritz--defun (name args &rest body)
   (declare (indent defun))
-  `(cl-symbol-macrolet ((i (spritz-i ,var))
-                        (j (spritz-j ,var))
-                        (k (spritz-k ,var))
-                        (a (spritz-a ,var))
-                        (z (spritz-z ,var))
-                        (w (spritz-w ,var)))
-     (cl-macrolet ((s (v) `(aref (spritz-s ,',var) (mod ,v 256))))
-       ,@body)))
+  (let ((func-name (intern (concat "spritz--" (symbol-name name)))))
+    `(defun ,func-name (this ,@args)
+       (cl-symbol-macrolet ((i (spritz--i this))
+                            (j (spritz--j this))
+                            (k (spritz--k this))
+                            (a (spritz--a this))
+                            (z (spritz--z this))
+                            (w (spritz--w this)))
+         (cl-macrolet ((s (v) `(aref (spritz--s this) (mod ,v 256)))
+                       (absorb-byte (b) `(spritz--absorb-byte this ,b))
+                       (absorb-nibble (x) `(spritz--absorb-nibble this ,x))
+                       (absorb-stop () `(spritz--absorb-stop this))
+                       (shuffle () `(spritz--shuffle this))
+                       (whip (r) `(spritz--whip this r))
+                       (crush () `(spritz--crush this))
+                       (squeeze (r) `(spritz--squeeze this r))
+                       (drip () `(spritz--drip this))
+                       (update () `(spritz--update this))
+                       (output () `(spritz--output this)))
+           ,@body)))))
 
 (defun spritz-create (&optional key)
   (let ((spritz (spritz--create)))
@@ -30,73 +54,64 @@
       (spritz-absorb spritz key))
     spritz))
 
-(defun spritz-absorb (spritz string)
+(spritz--defun absorb (string)
   (dotimes (v (length string))
-    (spritz--absorb-byte spritz (aref string v))))
+    (absorb-byte (aref string v))))
 
-(defun spritz--absorb-byte (spritz b)
-  (spritz--absorb-nibble spritz (logand b #x0f))
-  (spritz--absorb-nibble spritz (lsh b -4)))
+(spritz--defun absorb-byte (b)
+  (absorb-nibble (logand b #x0f))
+  (absorb-nibble (lsh b -4)))
 
-(defun spritz--absorb-nibble (spritz x)
-  (spritz--body spritz
-    (when (= a 128)
-      (spritz--shuffle spritz))
-    (rotatef (s a) (s (+ 128 x)))))
+(spritz--defun absorb-nibble (x)
+  (when (= a 128)
+    (shuffle))
+  (rotatef (s a) (s (+ 128 x))))
 
-(defun spritz--absorb-stop (spritz)
-  (spritz--body spritz
-    (when (= a 128)
-      (spritz--shuffle spritz))
-    (incf a)))
+(spritz--defun absorb-stop ()
+  (when (= a 128)
+    (shuffle))
+  (incf a))
 
-(defun spritz--shuffle (spritz)
-  (spritz--body spritz
-    (prog1 spritz
-      (spritz--whip spritz (* 2 256))
-      (spritz--crush spritz)
-      (spritz--whip spritz (* 2 256))
-      (spritz--crush spritz)
-      (spritz--whip spritz (* 2 256))
-      (setf a 0))))
+(spritz--defun shuffle ()
+  (prog1 this
+    (whip (* 2 256))
+    (crush)
+    (whip (* 2 256))
+    (crush)
+    (whip (* 2 256))
+    (setf a 0)))
 
-(defun spritz--whip (spritz r)
-  (spritz--body spritz
-    (dotimes (v r)
-      (spritz--update spritz))
-    (cl-loop do (incf w)
-             while (= 1 (gcd w 256)))))
+(spritz--defun whip (r)
+  (dotimes (v r)
+    (update))
+  (cl-loop do (incf w)
+           while (= 1 (gcd w 256))))
 
-(defun spritz--crush (spritz)
-  (let ((s (spritz-s spritz)))
-    (dotimes (v 128)
-      (when (> (aref s v) (aref s (- 255 v)))
-        (cl-rotatef (aref s v) (aref s (- 255 v)))))))
+(spritz--defun crush ()
+  (dotimes (v 128)
+    (when (> (s v) (s (- 255 v)))
+      (cl-rotatef (s v) (s (- 255 v))))))
 
-(defun spritz--squeeze (spritz r)
-  (spritz--body spritz
-    (when (> a 0)
-      (spritz--shuffle spritz))
-    (let ((p (make-string r 0)))
-      (dotimes (v r p)
-        (setf (aref p v (spritz--drip spritz)))))))
+(spritz--defun squeeze (r)
+  (when (> a 0)
+    (shuffle))
+  (let ((p (make-string r 0)))
+    (dotimes (v r p)
+      (setf (aref p v (drip))))))
 
-(defun spritz--drip (spritz)
-  (spritz--body spritz
-    (when (> a 0)
-      (spritz--shuffle spritz))
-    (spritz--update spritz)
-    (spritz--output spritz)))
+(spritz--defun drip ()
+  (when (> a 0)
+    (shuffle))
+  (update)
+  (output))
 
-(defun spritz--update (spritz)
-  (spritz--body spritz
-    (incf i w)
-    (setf j (+ k (s (+ j (s i)))))
-    (setf k (+ i k (s j)))
-    (rotatef (s i) (s j))))
+(spritz--defun update ()
+  (incf i w)
+  (setf j (+ k (s (+ j (s i)))))
+  (setf k (+ i k (s j)))
+  (rotatef (s i) (s j)))
 
-(defun spritz--output (spritz)
-  (spritz--body spritz
-    (setf z (s (+ j (s (+ i (s (+ z k)))))))))
+(spritz--defun output ()
+  (setf z (s (+ j (s (+ i (s (+ z k))))))))
 
-(spritz-create "foo")
+;(spritz-create "foo")
